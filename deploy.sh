953269243459878bf7901dcfe700be03b07edf63
ej <crazy979797@gmail.com>
2021-07-24 21:37:35 +0800

    edit repo name

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chenej9797/GameBoy.git master:gh-pages
+git push -f git@github.com:chenej9797/css_game_boy.git master:gh-pages
 
 cd -
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/GameBoy/'
+    publicPath: '/css_game_boy/'
   },
   module: {
     rules: [
